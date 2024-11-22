ecbf3b68fbb1074a43352e4333679471d954c466
Aaron Garcia <axyscreativeagency@gmail.com>
2024-11-21 22:28:24 -0700

    nav progress, favicon test

--- src/static/javascript/global/nav.js
+++ src/static/javascript/global/nav.js
@@ -3,31 +3,35 @@ const siteNav = document.querySelector(".site-nav"),
   mainContent = document.querySelector(".main-content");
 
 const navLinks = document.querySelectorAll(".nav-link"),
-  navFooterLinks = document.querySelectorAll(".nav-footer-link"),
   tabElementsPage = document.querySelectorAll(".tab-element-page"),
   tabElementsNav = document.querySelectorAll(".tab-element-nav");
 
 tabElementsNav.forEach((elem) => elem.setAttribute("tabIndex", "-1"));
 
+let isNavOpen;
+
 const toggleNav = () => {
-  const isNavOpen = siteNav.getAttribute("aria-hidden") === "true";
+  isNavOpen = siteNav.classList.contains("site-nav--active");
 
   siteNav.classList.toggle("site-nav--active");
   mainContent.classList.toggle("main-content--nav-active");
 
-  siteNav.setAttribute("aria-hidden", !isNavOpen);
-  navBtn.setAttribute("aria-expanded", isNavOpen);
+  // Set aria-hidden based on the new state of the navigation
+  siteNav.setAttribute("aria-hidden", isNavOpen ? "true" : "false");
+
+  // Update aria-expanded, aria-pressed, and aria-label on the nav button
+  navBtn.setAttribute("aria-expanded", isNavOpen ? "false" : "true");
   navBtn.setAttribute(
     "aria-label",
-    isNavOpen ? "Close navigation menu" : "Open navigation menu"
+    isNavOpen ? "Open navigation menu" : "Close navigation menu"
   );
 
   // Update tabindex for tabElementsPage and tabElementsNav
   tabElementsPage.forEach((el) =>
-    el.setAttribute("tabindex", isNavOpen ? "-1" : "0")
+    el.setAttribute("tabindex", isNavOpen ? "0" : "-1")
   );
   tabElementsNav.forEach((el) =>
-    el.setAttribute("tabindex", isNavOpen ? "0" : "-1")
+    el.setAttribute("tabindex", isNavOpen ? "-1" : "0")
   );
 };
 
@@ -43,10 +47,17 @@ const closeNav = () => {
   tabElementsNav.forEach((el) => el.setAttribute("tabindex", "-1"));
 };
 
-[...navLinks, ...navFooterLinks].forEach((link) => {
-  if (!link.classList.contains("prevent-nav-close")) {
-    link.addEventListener("click", closeNav);
-  }
-});
+// Add or remove the event listener for closing nav by clicking on main content
+if (!isNavOpen) {
+  mainContent.addEventListener("click", closeNav);
+} else {
+  mainContent.removeEventListener("click", closeNav);
+}
+
+// navLinks.forEach((link) => {
+//   if (!link.classList.contains("prevent-nav-close")) {
+//     link.addEventListener("click", closeNav);
+//   }
+// });
 
 navBtn.addEventListener("click", toggleNav);
