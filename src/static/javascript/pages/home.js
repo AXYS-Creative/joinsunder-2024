029953ec7075de27a810b3901fe31dcf456e8636
Aaron Garcia <axyscreativeagency@gmail.com>
2024-11-29 21:41:25 -0700

    scroll attempt for home page

--- src/static/javascript/pages/home.js
+++ src/static/javascript/pages/home.js
@@ -17,3 +17,48 @@ window.addEventListener("load", () => {
 });
 
 // console.log(mqMouse ? "mouse device" : "touch device");
+
+//
+// Image Reel Logic
+//
+
+let isPlaying = false;
+let shouldScroll = false;
+let wasSmallerScreen = false;
+const mediaQuery = window.matchMedia("(min-width: 1025px)");
+
+const handleSmallerScreens = (e) => {
+  if (e.matches) {
+    if (!isPlaying) {
+      isPlaying = true;
+      shouldScroll = true;
+      const pageScroll = () => {
+        if (shouldScroll) {
+          window.scrollBy(0, 1);
+          // setTimeout(pageScroll, 32);
+          setTimeout(pageScroll, 35);
+        }
+      };
+      setTimeout(() => {
+        pageScroll();
+      }, 1200);
+    }
+    wasSmallerScreen = false;
+  } else {
+    if (isPlaying) {
+      isPlaying = false;
+      shouldScroll = false;
+      wasSmallerScreen = true;
+      mediaQuery.removeEventListener("change", handleSmallerScreens);
+    }
+  }
+};
+
+mediaQuery.addEventListener("change", () => {
+  if (wasSmallerScreen && mediaQuery.matches) {
+    location.reload();
+  }
+  handleSmallerScreens(mediaQuery);
+});
+
+handleSmallerScreens(mediaQuery);
