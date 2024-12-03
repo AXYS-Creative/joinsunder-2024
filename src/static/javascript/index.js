9b51f2ca73b78510520813ebf4631b1c2918cb37
Aaron Garcia <axyscreativeagency@gmail.com>
2024-12-03 12:11:19 -0700

    removed unused imports, index.js

--- src/static/javascript/index.js
+++ src/static/javascript/index.js
@@ -1,7 +1,5 @@
 import "./global/animations.js";
-import "./global/magnet.js";
 import "./global/nav.js";
-import "./global/scroll.js";
 
 import "./components/cta.js";
 import "./components/video-player.js";
